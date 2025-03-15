2966e24ac03870e87e5e0a722a1b61f499322419
Yuri Braga <37191972+yuribraga17@users.noreply.github.com>
2025-03-14 23:30:28 -0300

    Update scanner.py

--- core/scanner.py
+++ core/scanner.py
@@ -44,22 +44,22 @@ def scan_file(file_path, patterns, malicious_hashes, virustotal_api_key):
         # Verificação de hash
         file_hash = hashlib.sha256(content.encode()).hexdigest()
         if file_hash in malicious_hashes:
-            log_entries.append(f"[{translate('alert')}] {translate('hash_malicious')} {file_hash} {translate('file')} {file_path}")
+            log_entries.append((f"[{translate('alert')}] {translate('hash_malicious')} {file_hash}", file_path, None, None))
 
         # Verificação com VirusTotal
         virustotal_result = check_hash_with_virustotal(file_hash, virustotal_api_key)
         if virustotal_result:
-            log_entries.append(virustotal_result)
+            log_entries.append((virustotal_result, file_path, None, None))
 
         # Detecção de ofuscação
         obfuscation_result = detect_obfuscation(content)
         if obfuscation_result:
-            log_entries.append(obfuscation_result)
+            log_entries.append((obfuscation_result, file_path, None, None))
 
         # Análise de comportamento
         behavior_result = analyze_behavior(content)
         if behavior_result:
-            log_entries.append(behavior_result)
+            log_entries.append((behavior_result, file_path, None, None))
 
         # Verificação de padrões suspeitos
         lines = content.splitlines()  # Divide o conteúdo em linhas
@@ -70,10 +70,10 @@ def scan_file(file_path, patterns, malicious_hashes, virustotal_api_key):
                 if line_number <= len(lines):  # Verifica se o número da linha é válido
                     line = lines[line_number - 1].strip()
                     log_entry = (
-                        f"[{translate('alert')}] {translate('suspicious_pattern')}: {pattern}\n"
-                        f"{translate('file')}: {file_path}\n"
-                        f"{translate('line')}: {line_number}\n"
-                        f"{translate('code')}: {line}\n"
+                        f"[{translate('alert')}] {translate('suspicious_pattern')}: {pattern}",
+                        file_path,
+                        line_number,
+                        line
                     )
                     log_entries.append(log_entry)
                     save_result(file_path, pattern)
